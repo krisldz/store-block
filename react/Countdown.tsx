8b74951b243fce43cf2971f39231549bf8287b10
Krzysztof Sokolowski <krzysztof.sokolowski@goodylabs.com>
2020-07-30 16:03:51 +0200

    countdown

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
-interface CountdownProps { }
+interface CountdownProps {}
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
-}
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
+  return (
+    <div>
+      <h1>Countdown</h1>
+    </div>
+  );
+};
 
 Countdown.schema = {
-  title: 'editor.countdown.title',
-  description: 'editor.countdown.description',
-  type: 'object',
+  title: "editor.countdown.title",
+  description: "editor.countdown.description",
+  type: "object",
   properties: {},
-}
+};
 
-export default Countdown
+export default Countdown;
